d2118226eaf0cabb31dc0526a105e80751de5013
eddie120678 <50688735+eddie120678@users.noreply.github.com>
2019-05-30 19:19:00 -0400

    Handler now handles events

--- util/handler.js
+++ util/handler.js
@@ -21,3 +21,14 @@ module.exports.loadCommands = () =>{
     })
   })
 }
+
+module.exports.loadEvents = () =>{
+  fs.readdir("./events", (err, files)=>{
+    if(err) return console.log(err);
+    let file = files.filter(f => f.split('.').pop() === "js");
+
+    file.forEach(f=>{
+      let prop = require(`../events/${f}`)
+    })
+  })
+}
