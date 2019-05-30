bed22ea1972bbc1f9275ecf3f46764f88041d468
eddie120678 <50688735+eddie120678@users.noreply.github.com>
2019-05-30 00:31:16 -0400

    just starting

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client(disableEveryone:true);
+const bot = new Discord.Client({disableEveryone:true});
 const config = require("./config.json");
 
-bot.login(config.token)
+bot.on("ready", () => {
+  console.log("Bot is online!");
+  bot.user.setActivity("Developing")
+});
+
+bot.on("message", async (message) => {
+  if(message.author.bot) return;
+  let prefix = config.prefix;
+
+  let args = message.content.slice(prefix.length).trim().split(/ +/)
+  let cmd = args.shift().tolowerCase();
+
+  if(cmd ==="ping") {
+    let botping = new Date() - message.createdAt
+    let apiping = bot.ping
+
+    return message.channel.send(`bot ping: ${botping}ms\nApi ping: ${apiping}ms`)
+  }
+});
+
+bot.login(config.token);
