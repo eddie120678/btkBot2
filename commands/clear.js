e551455fae3069ffeae062eff483dc8302d18859
eddie120678 <50688735+eddie120678@users.noreply.github.com>
2019-06-02 23:31:49 -0400

    removed async from clear command

--- commands/clear.js
+++ commands/clear.js
@@ -8,12 +8,18 @@ module.exports.command = {
   usage: "clear <number of messages"
 }
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("oof");
-  if(!args[0]) return message.channel.send("oof");
-  message.channel.bulkDelete(args[0] + 1).then(() => {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("I don't go around telling you telling me what to do. So don't go around telling me I am telling you telling me what to do!");
+  if(!args[0]) return message.channel.send("Did you want me to do something. You might want to put a 1 and a couple zeros in front of that");
+
+  try{
+    message.channel.bulkDelete(args[0] + 1).then(() => {
     message.channel.send(`Cleard ${args[0]} messages.`).then(msg => msg.delete(5000));
-  });
+    });
+  }
+  catch(e){
+    console.log(e)
+  }
 
 }
